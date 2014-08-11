dffae230e37120908181f6ac90084e3647d34f40
JohnRMoreau <John.R.Moreau@gmail.com>
2014-08-10 19:51:40 -0500

    Committing changes to extract code and including reference tables

---
+++ extract.R
@@ -0,0 +1,19 @@
+## Command to extract data from a frame and rename it
+
+## Future improvements
+##    check that everything in name file is character data and not factors
+##    Import the files from the disk
+##    Check to see if any ref names are not in the file
+##    Ask what the unique ID is, if any, and assign to the rownames
+##    Vectorize the for statement, use sapply
+
+extract <- function(datafile, namefile){
+  extvect <- which(colnames(datafile) %in% namefile[,1])  ## Get a vector of columns to pull
+  extdf <- datafile[,extvect]                             ## Extract the desired columns
+  namelen <- nrow(namefile)                             ## #Cols in name file - don't assume all names are in data
+  for (i in 1:namelen) {
+    coln  <- match(namefile[i,1],colnames(extdf))        ##Doesn't assume same order, gets the col num for the data
+    colnames(extdf)[coln] <- as.character(namefile[i,2])
+  }
+  extdf
+}
